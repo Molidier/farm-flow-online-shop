2b52c8500f8557818ee67d80f4de9777ee73dda4
Yekaterina Korobeinikova <kate.korobeinikova@gmail.com>
2024-11-12 00:04:19 +0500

    admin files updated

--- farm_flow/ffapp/admin.py
+++ farm_flow/ffapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Test
 
-# Register your models here.
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'one')
+    search_fields = ('one',)
+
+# Register the model with the custom admin class
+admin.site.register(Test, TestAdmin)
--- farm_flow/users/admin.py
+++ farm_flow/users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Buyer, Farmer, OTP
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    # Define the fields to display in the list view
+    list_display = ('email', 'phone_number', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'role')
+    search_fields = ('email', 'phone_number', 'first_name', 'last_name')
+    ordering = ('email',)
+    
+    # Define fieldsets for detailed view in the admin
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'role')}),
+    )
+    
+    # Define fieldsets for the add view in the admin
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone_number', 'first_name', 'last_name', 'role', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'deliveryAdress')
+    search_fields = ('user__phone_number', 'user__email', 'deliveryAdress')
+
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'Fname', 'is_verified')
+    search_fields = ('user__phone_number', 'user__email', 'Fname')
+    list_filter = ('is_verified',)
+
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'otp', 'created_at')
+    search_fields = ('phone_number',)
+    list_filter = ('created_at',)
+
+# Register the models with the custom admin classes
+admin.site.register(User, UserAdmin)
+admin.site.register(Buyer, BuyerAdmin)
+admin.site.register(Farmer, FarmerAdmin)
+admin.site.register(OTP, OTPAdmin)
