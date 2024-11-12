7cc50dd9a3169ee29902222fb5fa168fb40a7b5b
Yekaterina Korobeinikova <kate.korobeinikova@gmail.com>
2024-11-12 22:06:45 +0500

    hide auth and groups

--- farm_flow/ffapp/admin.py
+++ farm_flow/ffapp/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from .models import Test
+# from django.contrib import admin
+# from .models import Test
 
-class TestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'one')
-    search_fields = ('one',)
+# class TestAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'one')
+#     search_fields = ('one',)
 
-# Register the model with the custom admin class
-admin.site.register(Test, TestAdmin)
+# # Register the model with the custom admin class
+# admin.site.register(Test, TestAdmin)
--- farm_flow/users/admin.py
+++ farm_flow/users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Buyer, Farmer, OTP
+from rest_framework.authtoken.models import TokenProxy
+from django.contrib.auth.models import Group
+
+#Hides Auth Token and Groups Sections
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
 
 class UserAdmin(BaseUserAdmin):
     # Define the fields to display in the list view
