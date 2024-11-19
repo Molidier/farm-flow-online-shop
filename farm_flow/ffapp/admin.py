8c4c473a676a0da3426c2d7e4b1e94892be0f323
Yekaterina Korobeinikova <kate.korobeinikova@gmail.com>
2024-11-19 23:22:56 +0500

    delete duplicate

--- farm_flow/ffapp/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-# from .models import Test
-
-# class TestAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'one')
-#     search_fields = ('one',)
-
-# # Register the model with the custom admin class
-# admin.site.register(Test, TestAdmin)
--- farm_flow/ffapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Test(models.Model):
-    one=models.CharField()
-
-    
